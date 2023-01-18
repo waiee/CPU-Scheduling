45362ac1db5c052e0e4cb0bf8b54b05b1a437b20
waiee <waiee_z@yahoo.com>
2023-01-18 09:36:11 +0800

    test

--- algo/sjfPython.py
+++ algo/sjfPython.py
@@ -9,8 +9,9 @@ def get_num_processes():
     num_processes = int(input("Enter the number of processes: "))
     print()
     if 3 <= num_processes <= 10:
-        print("Please enter number 3-10.")
         return num_processes
+    else:
+        print("Please enter number 3-10.")
 
 # Prompt the user for the details of each process
 def get_process_details(num_processes):
